e3ffd76f4607859c9dfd2b4855313bf2643d46d2
Riajul-Islam-EWU <ritewu2014@gmail.com>
2022-11-14 21:35:52 +0600

    Worked in route and logout.

--- routes/web.php
+++ routes/web.php
@@ -16,7 +16,7 @@ use Illuminate\Support\Facades\Route;
 
 Auth::routes();
 
-Route::get('/', [App\Http\Controllers\HomeController::class, 'root'])->name('root');
+Route::get('/index', [App\Http\Controllers\HomeController::class, 'root'])->name('root');
 // Route::get('/index', [App\Http\Controllers\HomeController::class, 'root'])->name('root');
 
 //Update User Details
